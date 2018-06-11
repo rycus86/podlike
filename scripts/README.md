46c9d2d00a030f9d1a9fb127a4dee355c85ba398
Viktor Adam <rycus86@gmail.com>
2018-06-11 23:28:10 +0100

    podtemplate deploy sub-command

--- scripts/README.md
+++ scripts/README.md
@@ -1,5 +1,7 @@
 # Podlike scripts
 
+Command line shell wrapper scripts for common helpers of Podlike.
+
 ## podtemplate
 
 A shell script to invoke the template generator command of the application.
@@ -25,4 +27,47 @@ Install it as:
 $ curl -fsSL https://raw.githubusercontent.com/rycus86/podlike/master/scripts/podtemplate.sh | tee /usr/local/bin/podtemplate && chmod +x /usr/local/bin/podtemplate
 ```
 
-*You may need to add `sudo` in front of `tee` and `chmod`.*
+You *may* need to add `sudo` in front of `tee` and `chmod`, like:
+
+```shell
+$ curl -fsSL https://raw.githubusercontent.com/rycus86/podlike/master/scripts/podtemplate.sh | sudo tee /usr/local/bin/podtemplate && sudo chmod +x /usr/local/bin/podtemplate
+```
+
+### Sub-commands
+
+You can also use:
+
+```shell
+$ podtemplate template ...
+```
+
+This basically ends up doing the same as above.
+
+In the end, the generated results are used for deploying Swarm stacks, so the `podtemplate deploy` command can come in handy for automation:
+
+```shell
+$ podtemplate deploy ...  # use `docker stack deploy` args
+```
+
+This sub-command transforms the input templates, then deploys the result stack in Swarm.
+
+```
+Usage:	podtemplate deploy [OPTIONS] STACK
+
+Loads the given templated YAML files, transforms them,
+then deploys a new stack or updates an existing stack.
+
+Options:
+  -c, --compose-file strings   Path to a Compose file
+      --prune                  Prune services that are no longer referenced
+      --resolve-image string   Query the registry to resolve image digest and supported platforms ("always"|"changed"|"never") (default "always")
+      --with-registry-auth     Send registry authentication details to Swarm agents
+```
+
+### Limitations
+
+The template generator runs in a container, with the current working directory mounted and set as the working directory within. Because of this, paths pointing outside of this area will most probably not work, like:
+
+```shell
+$ podtemplate ../../outside/of/working/area /var/different/absolute/path
+```
--- scripts/podtemplate.sh
+++ scripts/podtemplate.sh
@@ -2,8 +2,101 @@
 
 TAG=${PODLIKE_VERSION:-latest}
 
-docker run --rm -i          \
-    -v $PWD:/workspace:ro   \
-    -w /workspace           \
-    rycus86/podlike:${TAG}  \
-    template $@
+print_deploy_help() {
+    echo """
+Usage:	podtemplate deploy [OPTIONS] STACK
+
+Loads the given templated YAML files, transforms them,
+then deploys a new stack or updates an existing stack.
+
+Options:
+  -c, --compose-file strings   Path to a Compose file
+      --prune                  Prune services that are no longer referenced
+      --resolve-image string   Query the registry to resolve image digest and supported platforms (\"always\"|\"changed\"|\"never\") (default \"always\")
+      --with-registry-auth     Send registry authentication details to Swarm agents
+    """
+}
+
+exec_deploy() {
+    COMPOSE_FILES=""
+    STACK_DEPLOY_ARGS=""
+    while [ $# -gt 0 ]
+    do
+    key="$1"
+
+    case $key in
+        -h|--help)
+        print_deploy_help
+        exit 0
+        ;;
+        -c|--compose-file)
+        COMPOSE_FILES="$COMPOSE_FILES $2"
+        shift
+        shift
+        ;;
+        --resolve-image)
+        STACK_DEPLOY_ARGS="$STACK_DEPLOY_ARGS $key $2"
+        shift
+        shift
+        ;;
+        --prune|--with-registry-auth)
+        STACK_DEPLOY_ARGS="$STACK_DEPLOY_ARGS $key"
+        shift
+        ;;
+        --bundle-file)
+        echo 'Deploying Bundle files is not supported'
+        exit 1
+        ;;
+        *)
+        STACK_DEPLOY_ARGS="$STACK_DEPLOY_ARGS $key"
+        shift
+        ;;
+    esac
+    done
+
+    if [ "$COMPOSE_FILES" = "" ]; then
+        print_deploy_help
+        exit 1
+    fi
+
+    # make sure the target image exist
+    docker image inspect -f . rycus86/podlike:${TAG} 2>/dev/null >/dev/null || docker pull rycus86/podlike:${TAG}
+
+    # generate the YAML ouput from the templates
+    CONVERTED=$(docker run --rm -i -v $PWD:/workspace:ro -w /workspace rycus86/podlike:${TAG} template $COMPOSE_FILES 2>&1)
+    if [ "$?" != "0" ]; then
+        echo "$CONVERTED"
+        exit 1
+    fi
+
+    # do the actual stack deployment
+    echo """$CONVERTED""" | docker stack deploy -c - $STACK_DEPLOY_ARGS
+}
+
+exec_print() {
+    # make sure the target image exist
+    docker image inspect -f . rycus86/podlike:${TAG} 2>/dev/null >/dev/null || docker pull rycus86/podlike:${TAG}
+
+    # generate the YAML ouput from the templates
+    docker run --rm -i          \
+        -v $PWD:/workspace:ro   \
+        -w /workspace           \
+        rycus86/podlike:${TAG}  \
+        template $@   2>&1
+}
+
+# podtemplate deploy ...
+if [ "$1" = "deploy" ]; then
+    shift  # drop the deploy parameter
+    exec_deploy $@
+
+# podtemplate template ...
+elif [ "$1" = "template" ]; then
+    shift  # drop the deploy parameter
+    exec_print $@
+
+# podtemplate ...
+else
+    exec_print $@
+
+fi
