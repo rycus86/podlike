55228587b4df167d1dea027989b764414149de77
Kacper Donat <kadet1090@gmail.com>
2022-10-28 18:11:08 +0200

    Add support for private registries
    This commit adds support for private registries, using
    /var/run/secrets/dockerregistryauth.json file. This file should be
    compatible with docker's config.json or dockerconfigjson secret from
    kubernetes [1]
    
    [1]: https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/
    
    Fixes #6

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -1,9 +1,15 @@
 package config
 
+import "github.com/docker/docker/api/types"
+
 type Configuration struct {
 	SharePids    bool
 	ShareIpc     bool
 	ShareVolumes bool
 	StreamLogs   bool
 	AlwaysPull   bool
+}
+
+type RegistryAuth struct {
+	Auths map[string]types.AuthConfig `json:"auths"`
 }
